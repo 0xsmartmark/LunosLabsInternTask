1afe1ef558942a9eb0cdc7250ce7bf58f969e5e0
0xsmartmark <0xsmartmark@gmail.com>
2023-09-10 19:39:43 +0800

    final version of scripts for L0 (I hope)

--- test_3_hardhat/contracts/MyOFT.sol
+++ test_3_hardhat/contracts/MyOFT.sol
@@ -6,5 +6,7 @@ import "@layerzerolabs/solidity-examples/contracts/token/oft/OFT.sol";
 
 // override decimal() function is needed
 contract MyOFT is OFT {
-    constructor(string memory _name, string memory _symbol, address _lzEndpoint) OFT(_name, _symbol, _lzEndpoint) {}
+    constructor(string memory _name, string memory _symbol, address _lzEndpoint) OFT(_name, _symbol, _lzEndpoint) {
+        _mint(msg.sender, 100 * (10 ** decimals()));
+    }
 }
--- test_3_hardhat/hardhat.config.ts
+++ test_3_hardhat/hardhat.config.ts
@@ -6,11 +6,14 @@ import '@nomicfoundation/hardhat-chai-matchers'
 
 import 'dotenv/config';
 
-const SEPOLIA_PRIVATE_KEY  = process.env.PRIVATE_KEY as string;
+const PRIVATE_KEY  = process.env.PRIVATE_KEY as string;
 const ALCHEMY_SEPOLIA_API_KEY = process.env.ALCHEMY_SEPOLIA_URL as string;
+const ALCHEMY_ARBITRUM_GEORLI_URL = process.env.ALCHEMY_ARBITRUM_URL as string;
+const ALCHEMY_MUMBAI_URL = process.env.ALCHEMY_MUMBAI_URL as string;
 
-const OPTIMISM_GEORLI_PRIVATE_KEY  = process.env.PRIVATE_KEY as string;;
-const ALCHEMY_OPTIMISM_GEORLI_API_KEY = process.env.ALCHEMY_OPTIMISM_URL as string;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY as string;
+const ARBISCAN_API_KEY = process.env.ARBISCAN_API_KEY as string;
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY as string;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
@@ -18,12 +21,25 @@ const config: HardhatUserConfig = {
   networks: {
     eth_sepolia_testnet: {
       url: ALCHEMY_SEPOLIA_API_KEY,
-      accounts: [SEPOLIA_PRIVATE_KEY]
+      accounts: [PRIVATE_KEY]
     },
 
-    optimism_goerli_testnet: {
-      url: ALCHEMY_OPTIMISM_GEORLI_API_KEY,
-      accounts: [OPTIMISM_GEORLI_PRIVATE_KEY]
+    arbitrum_goerli_testnet: {
+      url: ALCHEMY_ARBITRUM_GEORLI_URL,
+      accounts: [PRIVATE_KEY]
+    },
+
+    mumbai: {
+      url: ALCHEMY_MUMBAI_URL,
+      accounts: [PRIVATE_KEY]
+    } 
+  }, 
+
+  etherscan: {
+    apiKey: {
+      eth_sepolia_testnet: ETHERSCAN_API_KEY,
+      arbitrumGoerli: ARBISCAN_API_KEY,
+      polygonMumbai: POLYGONSCAN_API_KEY
     }
   }
 };
